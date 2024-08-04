060a4e034b96c3ab23e75e5f0c28abf1bdeadc97
LukaLazaraS <lazarashvili02@gmail.com>
2024-08-05 03:34:00 +0400

    movie & review class

---
+++ src/main/java/dev/firstSpringProject/Movie.java
@@ -0,0 +1,27 @@
+package dev.firstSpringProject;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "movie")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+  @Id
+  private ObjectId id;
+  private String title;
+  private String ImdbId;
+  private String releaseDate;
+  private String trailerLink;
+  private String poster;
+  private List<String> genres;
+  private List<String> backdrops;
+  private List<Review> reviewIds;
+}
--- src/main/java/dev/firstSpringProject/MoviesApplication.java
+++ src/main/java/dev/firstSpringProject/MoviesApplication.java
@@ -1,4 +1,4 @@
-package dev.firstSpringProject.movies;
+package dev.firstSpringProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
